494c8a5ba02dae4501abcfb821a8a7c1b37d8e3f
mindtap <michael.lesniak@cengage.com>
2021-12-09 13:07:16 -0500

    day9 part one complete

--- src/main/java/com/magicalpoet/advent/Day9LavaTubes.java
+++ src/main/java/com/magicalpoet/advent/Day9LavaTubes.java
@@ -1,9 +1,13 @@
 package com.magicalpoet.advent;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
+import java.util.ListIterator;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 public class Day9LavaTubes {
@@ -13,16 +17,89 @@ public class Day9LavaTubes {
         List<Integer> lowPoints = findLowPoints(cave);
         return calculateRiskLevel(lowPoints);
     }
+    public int findBasinsScore(String caveMap) {
+        String[] lines = caveMap.split("\n");
+        Map<Integer, List<Integer>> cave = getCaveFromStringMap(lines);
+        // findBasins(cave);
+        // get three largest
+        // return product of the size of largest
+        return 0;
+    }
 
     public int calculateRiskLevel(List<Integer> lowPoints) {
         return lowPoints.stream().mapToInt(Integer::intValue).sum() + lowPoints.size();
     }
 
     public List<Integer> findLowPoints(Map<Integer, List<Integer>> cave) {
-        return null;
+        List<Integer> lowPoints = new ArrayList<>();
+        // TODO: stream and collect?
+        cave.forEach((yPosition, value) -> {
+            ListIterator<Integer> iterator = value.listIterator();
+            while (iterator.hasNext()) {
+                final int xPosition = iterator.nextIndex();
+                final Integer point = iterator.next();
+                final List<Integer> neighbors = getNeighbors(xPosition, yPosition, cave);
+                if (isLowPoint(neighbors, point)) {
+                    lowPoints.add(point);
+                }
+            }
+        });
+        return lowPoints;
     }
 
-    private Map<Integer, List<Integer>> getCaveFromStringMap(String[] lines) {
+    public boolean isLowPoint(List<Integer> neighbors, Integer point) {
+        for (Integer neighbor : neighbors) {
+            if (point >= neighbor) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static List<Integer> getNeighbors(Integer xValue, Integer yValue, Map<Integer, List<Integer>> map) {
+        List<Integer> points = new ArrayList<>();
+        getNorthernNeighbor(xValue, yValue, map).ifPresent(points::add);
+        getWesternNeighbor(xValue, yValue, map).ifPresent(points::add);
+        getEasternNeighbor(xValue, yValue, map).ifPresent(points::add);
+        getSouthernNeighbor(xValue, yValue, map).ifPresent(points::add);
+        return points;
+    }
+
+    private static Optional<Integer> getNorthernNeighbor(Integer xValue, Integer yValue, Map<Integer, List<Integer>> map) {
+        if (map.containsKey(yValue - 1)) {
+            final List<Integer> integers = map.get(yValue - 1);
+            return Optional.of(integers.get(xValue));
+        }
+        return Optional.empty();
+    }
+
+    private static Optional<Integer> getEasternNeighbor(Integer xValue, Integer yValue, Map<Integer, List<Integer>> map) {
+        final List<Integer> integers = map.get(yValue);
+        try {
+            return Optional.of(integers.get(xValue + 1));
+        } catch (IndexOutOfBoundsException e) {
+            return Optional.empty();
+        }
+    }
+
+    private static Optional<Integer> getWesternNeighbor(Integer xValue, Integer yValue, Map<Integer, List<Integer>> map) {
+        final List<Integer> integers = map.get(yValue);
+        try {
+            return Optional.of(integers.get(xValue - 1));
+        } catch (IndexOutOfBoundsException e) {
+            return Optional.empty();
+        }
+    }
+
+    private static Optional<Integer> getSouthernNeighbor(Integer xValue, Integer yValue, Map<Integer, List<Integer>> map) {
+        if (map.containsKey(yValue + 1)) {
+            final List<Integer> integers = map.get(yValue + 1);
+            return Optional.of(integers.get(xValue));
+        }
+        return Optional.empty();
+    }
+
+    private static Map<Integer, List<Integer>> getCaveFromStringMap(String[] lines) {
         Map<Integer, List<Integer>> cave = new HashMap<>();
         int index = 0;
         for (String line : lines) {
@@ -32,7 +109,7 @@ public class Day9LavaTubes {
         return cave;
     }
 
-    private List<Integer> splitMapLine(String line) {
+    private static List<Integer> splitMapLine(String line) {
         String[] digits = line.split("");
         return Arrays.stream(digits).map(Integer::parseInt).collect(Collectors.toList());
     }
--- src/test/java/com/magicalpoet/advent/Day9LavaTubesTest.java
+++ src/test/java/com/magicalpoet/advent/Day9LavaTubesTest.java
@@ -3,9 +3,11 @@ package com.magicalpoet.advent;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class Day9LavaTubesTest {
 
@@ -20,16 +22,51 @@ class Day9LavaTubesTest {
     }
 
     @Test
-    void calculateRiskLevel() {
+    void calculatesRiskLevel() {
         List<Integer> input = List.of(2,6,9,0,7);
         int riskLevel = lavaTubes.calculateRiskLevel(input);
         assertThat(riskLevel).isEqualTo(29);
     }
 
     @Test
-    void partOneTestInput() throws Exception {
-        String caveMap = utils.readFileToString(TEST_INPUT_FILE);
-        assertThat(lavaTubes.reportRiskLevel(caveMap)).isEqualTo(15);
+    void isLowPoint() {
+        assertThat(lavaTubes.isLowPoint(List.of(2,3), 3)).isFalse();
+        assertThat(lavaTubes.isLowPoint(List.of(5,6), 3)).isTrue();
     }
 
+    @Test
+    void findsLowPoints() {
+        List<Integer> row1 = List.of(3, 6, 8, 9);
+        List<Integer> row2 = List.of(9, 3, 6, 4);
+        List<Integer> row3 = List.of(2, 3, 7, 5);
+        Map<Integer, List<Integer>> cave = new HashMap<>();
+        cave.put(0, row1);
+        cave.put(1, row2);
+        cave.put(2, row3);
+
+        final List<Integer> lowPoints = lavaTubes.findLowPoints(cave);
+
+        assertThat(lowPoints).containsExactlyInAnyOrder(3, 4, 2);
+    }
+
+    @Test
+    void partOneTestInput() throws Exception {
+        String caveMap = utils.readFileToString(TEST_INPUT_FILE);
+        final int riskLevel = lavaTubes.reportRiskLevel(caveMap);
+        assertThat(riskLevel).isEqualTo(15);
+    }
+
+    @Test
+    void partOneAnswer() throws Exception {
+        String caveMap = utils.readFileToString(INPUT_FILE);
+        final int riskLevel = lavaTubes.reportRiskLevel(caveMap);
+        assertThat(riskLevel).isEqualTo(550);
+    }
+
+    @Test
+    void partTwoTestInput() throws Exception {
+        String caveMap = utils.readFileToString(TEST_INPUT_FILE);
+        final int basinScore = lavaTubes.findBasinsScore(caveMap);
+        assertThat(basinScore).isEqualTo(1134);
+    }
 }
