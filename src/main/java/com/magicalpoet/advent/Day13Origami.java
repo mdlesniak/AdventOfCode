901eecf215bb11ea48fc6a269f451e32f5e8fc39
mindtap <michael.lesniak@cengage.com>
2021-12-13 17:21:25 -0500

    day13 completed

---
+++ src/main/java/com/magicalpoet/advent/Day13Origami.java
@@ -0,0 +1,105 @@
+package com.magicalpoet.advent;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Day13Origami {
+    public int countAfterFolding(String dots, String foldInstructions) {
+        final Map<String, Coordinate> foldedGrid = createPaperAndFold(dots, foldInstructions);
+        return foldedGrid.size();
+    }
+
+    public String visualizeAfterFolding(String dots, String foldInstructions) {
+        final Map<String, Coordinate> foldedGrid = createPaperAndFold(dots, foldInstructions);
+        return prettyPrint(foldedGrid);
+    }
+
+    public Map<String, Coordinate> createPaperAndFold(String dots, String foldInstructions) {
+        final String[] points = dots.split("\n");
+        Map<String, Coordinate> grid = plotPoints(points);
+        return foldPaper(grid, foldInstructions);
+    }
+
+    private String prettyPrint(Map<String, Coordinate> grid) {
+        StringBuilder picture = new StringBuilder();
+        for (int y = 0; y < 40; y++) {
+            for (int x = 0; x < 40; x++) {
+                String key = x + "," + y;
+                if (grid.containsKey(key)) {
+                    picture.append("#");
+                } else {
+                    picture.append("-");
+                }
+            }
+            picture.append("\n");
+        }
+        return picture.toString();
+    }
+
+    private Map<String, Coordinate> plotPoints(String[] points) {
+        Map<String, Coordinate> grid = new HashMap<>();
+        for (int i = 0, pointsLength = points.length; i < pointsLength; i++) {
+            String point = points[i];
+            final String[] numbers = point.split(",");
+            final Coordinate coordinate = new Coordinate(Integer.parseInt(numbers[0]), Integer.parseInt(numbers[1]));
+            grid.put(point, coordinate);
+        }
+        return grid;
+    }
+
+    private Map<String, Coordinate> foldPaper(Map<String, Coordinate> grid, String foldInstructions) {
+        final String[] instructions = foldInstructions.split("\n");
+        Map<String, Coordinate> result = grid;
+        for (String instruction : instructions) {
+            result = executeFold(result, instruction);
+        }
+        return result;
+    }
+
+    private Map<String, Coordinate> executeFold(Map<String, Coordinate> grid, String foldInstruction) {
+        final String[] instruction = foldInstruction.split("=");
+        Map<String, Coordinate> result;
+        if (instruction[0].endsWith("x")) {
+            result = foldAlongX(Integer.parseInt(instruction[1]), grid);
+        } else {
+            result = foldAlongY(Integer.parseInt(instruction[1]), grid);
+        }
+        return result;
+    }
+
+    private Map<String, Coordinate> foldAlongX(int value, Map<String, Coordinate> grid) {
+        Map<String, Coordinate> newGrid = new HashMap<>();
+        for (Coordinate coordinate : grid.values()) {
+            if (coordinate.x < value) {
+                newGrid.put(coordinate.x + "," + coordinate.y, coordinate);
+            } else {
+                Coordinate newCoordinate = new Coordinate(value - (coordinate.x - value), coordinate.y);
+                newGrid.put(newCoordinate.x + "," + newCoordinate.y, newCoordinate);
+            }
+        }
+        return newGrid;
+    }
+
+    private Map<String, Coordinate> foldAlongY(int value, Map<String, Coordinate> grid) {
+        Map<String, Coordinate> newGrid = new HashMap<>();
+        for (Coordinate coordinate : grid.values()) {
+            if (coordinate.y < value) {
+                newGrid.put(coordinate.x + "," + coordinate.y, coordinate);
+            } else {
+                Coordinate newCoordinate = new Coordinate(coordinate.x, value - (coordinate.y - value));
+                newGrid.put(newCoordinate.x + "," + newCoordinate.y, newCoordinate);
+            }
+        }
+        return newGrid;
+    }
+
+    public static class Coordinate {
+        protected int x;
+        protected int y;
+
+        Coordinate(int xValue, int yValue) {
+            this.x = xValue;
+            this.y = yValue;
+        }
+    }
+}
