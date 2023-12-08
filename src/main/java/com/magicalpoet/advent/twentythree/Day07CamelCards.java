2f453d90325b680a850032e7072fa779ea83c19e
Michael Lesniak <michael.lesniak@cengage.com>
2023-12-07 21:27:14 -0500

    Day 7 complete

--- src/main/java/com/magicalpoet/advent/twentythree/Day07CamelCards.java
+++ src/main/java/com/magicalpoet/advent/twentythree/Day07CamelCards.java
@@ -12,16 +12,32 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 public class Day07CamelCards {
-    private static final Map<String, Integer> cardMap = Map.of(
+    private static final Map<String, Integer> cardMapStandard = Map.of(
             "A", 14,
             "K", 13,
             "Q", 12,
             "J", 11,
             "T", 10
     );
+    private static final Map<String, Integer> cardMapJokers = Map.of(
+            "A", 14,
+            "K", 13,
+            "Q", 12,
+            "J", 0,
+            "T", 10
+    );
 
-    public int totalWinnings(List<String> input) {
-        List<Hand> hands = input.stream().map(this::createHand).collect(Collectors.toList());
+    public int totalWinningsStandard(List<String> input) {
+        List<Hand> hands = input.stream().map(this::createHandStandard).collect(Collectors.toList());
+        return getTotalWinnings(hands);
+    }
+
+    public int totalWinningsWithJokers(List<String> input) {
+        List<Hand> hands = input.stream().map(this::createHandWithJokers).collect(Collectors.toList());
+        return getTotalWinnings(hands);
+    }
+
+    private int getTotalWinnings(List<Hand> hands) {
         sortHands(hands);
         int multiplier = hands.size();
         int totalWinnings = 0;
@@ -32,13 +48,57 @@ public class Day07CamelCards {
         return totalWinnings;
     }
 
-    public Hand createHand(String input) {
-        Hand hand = new Hand(input);
-        hand.setRank(classifyHand(hand.getCards()));
+    private Hand createHandStandard(String data) {
+        Hand hand = new Hand(data);
+        hand.setRank(classifyHandStandard(hand.getCards()));
         return hand;
     }
 
-    public Rank classifyHand(String hand) {
+    private Hand createHandWithJokers(String data) {
+        Hand hand = new Hand(data);
+        hand.setRank(classifyHandWithJokers(hand.getCards()));
+        return hand;
+    }
+
+    public Rank classifyHandWithJokers(String hand) {
+        Rank rank = classifyHandStandard(hand);
+        int jokers = hand.length() - hand.replace("J", "").length();
+        switch (jokers) {
+            case 4 -> rank = Rank.FIVE_OF_A_KIND;
+            case 3 -> {
+                if (rank == Rank.THREE_OF_A_KIND) {
+                    rank = Rank.FOUR_OF_A_KIND;
+                } else {
+                    rank = Rank.FIVE_OF_A_KIND;
+                }
+            }
+            case 2 -> {
+                if (rank == Rank.ONE_PAIR) {
+                    rank = Rank.THREE_OF_A_KIND;
+                } else if (rank == Rank.TWO_PAIR) {
+                    rank = Rank.FOUR_OF_A_KIND;
+                } else {
+                    rank = Rank.FIVE_OF_A_KIND;
+                }
+            }
+            case 1 -> {
+                if (rank == Rank.HIGH_CARD) {
+                    rank = Rank.ONE_PAIR;
+                } else if (rank == Rank.ONE_PAIR) {
+                    rank = Rank.THREE_OF_A_KIND;
+                } else if (rank == Rank.TWO_PAIR) {
+                    rank = Rank.FULL_HOUSE;
+                } else if (rank == Rank.THREE_OF_A_KIND) {
+                    rank = Rank.FOUR_OF_A_KIND;
+                } else {
+                    rank = Rank.FIVE_OF_A_KIND;
+                }
+            }
+        }
+        return rank;
+    }
+
+    public Rank classifyHandStandard(String hand) {
         final String[] cards = hand.split("");
         Map<String, Integer> cardsCount = new HashMap<>();
         Arrays.stream(cards).forEach(card -> Utils.incrementMapAtKey(cardsCount, card));
@@ -117,7 +177,8 @@ public class Day07CamelCards {
             try {
                 return Integer.parseInt(card);
             } catch (Exception e) {
-                return cardMap.get(card);
+//                return cardMapStandard.get(card);
+                return cardMapJokers.get(card);
             }
         }
     }
--- src/test/java/com/magicalpoet/advent/twentythree/Day07CamelCardsTest.java
+++ src/test/java/com/magicalpoet/advent/twentythree/Day07CamelCardsTest.java
@@ -29,36 +29,29 @@ class Day07CamelCardsTest {
     }
 
     @Test
-    void classifiesHands() {
-        assertThat(camelCards.classifyHand("32T9K")).isEqualTo(HIGH_CARD);
-        assertThat(camelCards.classifyHand("32T3K")).isEqualTo(ONE_PAIR);
-        assertThat(camelCards.classifyHand("KK677")).isEqualTo(TWO_PAIR);
-        assertThat(camelCards.classifyHand("A8A88")).isEqualTo(FULL_HOUSE);
-        assertThat(camelCards.classifyHand("J7777")).isEqualTo(FOUR_OF_A_KIND);
-        assertThat(camelCards.classifyHand("AAAAA")).isEqualTo(FIVE_OF_A_KIND);
-        assertThat(camelCards.classifyHand("T55J5")).isEqualTo(THREE_OF_A_KIND);
+    void classifiesHandsStandard() {
+        assertThat(camelCards.classifyHandStandard("32T9K")).isEqualTo(HIGH_CARD);
+        assertThat(camelCards.classifyHandStandard("32T3K")).isEqualTo(ONE_PAIR);
+        assertThat(camelCards.classifyHandStandard("KK677")).isEqualTo(TWO_PAIR);
+        assertThat(camelCards.classifyHandStandard("A8A88")).isEqualTo(FULL_HOUSE);
+        assertThat(camelCards.classifyHandStandard("J7777")).isEqualTo(FOUR_OF_A_KIND);
+        assertThat(camelCards.classifyHandStandard("AAAAA")).isEqualTo(FIVE_OF_A_KIND);
+        assertThat(camelCards.classifyHandStandard("T55J5")).isEqualTo(THREE_OF_A_KIND);
     }
 
     @Test
-    void createsHand() {
-        final String input = "J7777 123";
-
-        Day07CamelCards.Hand hand = camelCards.createHand(input);
-
-        assertThat(hand.getCards()).isEqualTo("J7777");
-        assertThat(hand.getWager()).isEqualTo(123);
-        assertThat(hand.getRank()).isEqualTo(FOUR_OF_A_KIND);
-    }
-
-    @Test
-    void sortsHands() {
+    void sortsHandsStandard() {
         final List<String> input = List.of(
                 "32T3K 765",
                 "T55J5 684",
                 "KK677 28",
                 "KTJJT 220",
                 "QQQJA 483");
-        final List<Day07CamelCards.Hand> hands = input.stream().map(camelCards::createHand).collect(Collectors.toList());
+        final List<Day07CamelCards.Hand> hands = input.stream().map(input1 -> {
+            Day07CamelCards.Hand hand = new Day07CamelCards.Hand(input1);
+            hand.setRank(camelCards.classifyHandStandard(hand.getCards()));
+            return hand;
+        }).collect(Collectors.toList());
 
         camelCards.sortHands(hands);
 
@@ -71,17 +64,78 @@ class Day07CamelCardsTest {
 
     @Test
     void partOneTest() {
+        List<String> input = List.of(
+                "32T3K 765",
+                "T55J5 684",
+                "KK677 28",
+                "KTJJT 220",
+                "QQQJA 483");
+        assertThat(camelCards.totalWinningsStandard(input)).isEqualTo(6440);
+    }
+
+    @Test
+    void partOne() {
+        assertThat(camelCards.totalWinningsStandard(data)).isEqualTo(251806792);
+    }
+
+    @Test
+    void classifiesHandsWithJokers() {
+        assertThat(camelCards.classifyHandWithJokers("32T9K")).isEqualTo(HIGH_CARD);
+        assertThat(camelCards.classifyHandWithJokers("3JT9K")).isEqualTo(ONE_PAIR);
+        assertThat(camelCards.classifyHandWithJokers("3JT99")).isEqualTo(THREE_OF_A_KIND);
+        assertThat(camelCards.classifyHandWithJokers("KK677")).isEqualTo(TWO_PAIR);
+        assertThat(camelCards.classifyHandWithJokers("KKJ77")).isEqualTo(FULL_HOUSE);
+        assertThat(camelCards.classifyHandWithJokers("T55J5")).isEqualTo(FOUR_OF_A_KIND);
+        assertThat(camelCards.classifyHandWithJokers("KTJJT")).isEqualTo(FOUR_OF_A_KIND);
+        assertThat(camelCards.classifyHandWithJokers("QQQJA")).isEqualTo(FOUR_OF_A_KIND);
+        assertThat(camelCards.classifyHandWithJokers("JJ249")).isEqualTo(THREE_OF_A_KIND);
+        assertThat(camelCards.classifyHandWithJokers("JJJ99")).isEqualTo(FIVE_OF_A_KIND);
+        assertThat(camelCards.classifyHandWithJokers("JJ299")).isEqualTo(FOUR_OF_A_KIND);
+        assertThat(camelCards.classifyHandWithJokers("J7777")).isEqualTo(FIVE_OF_A_KIND);
+        assertThat(camelCards.classifyHandWithJokers("JJJ37")).isEqualTo(FOUR_OF_A_KIND);
+        assertThat(camelCards.classifyHandWithJokers("JJJ77")).isEqualTo(FIVE_OF_A_KIND);
+        assertThat(camelCards.classifyHandWithJokers("JJJJJ")).isEqualTo(FIVE_OF_A_KIND);
+    }
+
+    @Test
+    void sortsHandsWithJokers() {
         final List<String> input = List.of(
                 "32T3K 765",
                 "T55J5 684",
                 "KK677 28",
                 "KTJJT 220",
                 "QQQJA 483");
-        assertThat(camelCards.totalWinnings(input)).isEqualTo(6440);
+        final List<Day07CamelCards.Hand> hands = input.stream().map(input1 -> {
+            Day07CamelCards.Hand hand = new Day07CamelCards.Hand(input1);
+            hand.setRank(camelCards.classifyHandWithJokers(hand.getCards()));
+            return hand;
+        }).collect(Collectors.toList());
+
+        camelCards.sortHands(hands);
+
+        assertThat(hands.get(0).getCards()).isEqualTo("KTJJT");
+        assertThat(hands.get(1).getCards()).isEqualTo("QQQJA");
+        assertThat(hands.get(2).getCards()).isEqualTo("T55J5");
+        assertThat(hands.get(3).getCards()).isEqualTo("KK677");
+        assertThat(hands.get(4).getCards()).isEqualTo("32T3K");
+    }
+
+
+    @Test
+    void partTwoTest() {
+        List<String> input = List.of(
+                "32T3K 765",
+                "T55J5 684",
+                "KK677 28",
+                "KTJJT 220",
+                "QQQJA 483");
+        assertThat(camelCards.totalWinningsWithJokers(input)).isEqualTo(5905);
     }
 
     @Test
-    void partOne() {
-        assertThat(camelCards.totalWinnings(data)).isEqualTo(251806792);
+    void partTwo() {
+        assertThat(camelCards.totalWinningsWithJokers(data)).isEqualTo(252113488);
     }
+
+
 }
