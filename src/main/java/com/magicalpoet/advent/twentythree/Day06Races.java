17ee6be77f0480b2169621d0c23e9bfb96a2684c
Michael Lesniak <michael.lesniak@cengage.com>
2023-12-11 16:47:01 -0500

    Day 6 part 2 completed

--- src/main/java/com/magicalpoet/advent/twentythree/Day06Races.java
+++ src/main/java/com/magicalpoet/advent/twentythree/Day06Races.java
@@ -3,19 +3,19 @@ package com.magicalpoet.advent.twentythree;
 import java.util.Map;
 
 public class Day06Races {
-    public int scoreRaces(Map<Integer, Integer> race) {
-        int totalWays = 1;
+    public long scoreRaces(Map<Long, Long> race) {
+        long totalWays = 1;
         for (var entry : race.entrySet()) {
             totalWays = totalWays * waysToWin(entry.getKey(), entry.getValue());
         }
         return totalWays;
     }
 
-    public int waysToWin(Integer time, Integer recordDistance) {
-        int holdTime = time / 2;
-        int waysToWin = 0;
+    public long waysToWin(Long time, Long recordDistance) {
+        long holdTime = time / 2;
+        long waysToWin = 0;
         while (holdTime > 0) {
-            int distanceTraveled = holdTime * (time - holdTime);
+            long distanceTraveled = holdTime * (time - holdTime);
             if (distanceTraveled > recordDistance) {
                 waysToWin++;
                 holdTime--;
--- src/test/java/com/magicalpoet/advent/twentythree/Day06RacesTest.java
+++ src/test/java/com/magicalpoet/advent/twentythree/Day06RacesTest.java
@@ -7,28 +7,28 @@ import java.util.Map;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class Day06RacesTest {
-    private final Map<Integer, Integer> input = Map.of(
-            47, 282,
-            70, 1079,
-            75, 1147,
-            66, 1062
+    private final Map<Long, Long> input = Map.of(
+            47L, 282L,
+            70L, 1079L,
+            75L, 1147L,
+            66L, 1062L
     );
     private final Day06Races races = new Day06Races();
 
     @Test
     void findsWaysToWin() {
-        assertThat(races.waysToWin(7, 9)).isEqualTo(4);
-        assertThat(races.waysToWin(15, 40)).isEqualTo(8);
-        assertThat(races.waysToWin(30, 200)).isEqualTo(9);
-        assertThat(races.waysToWin(71530, 940200)).isEqualTo(71503);
+        assertThat(races.waysToWin(7L, 9L)).isEqualTo(4);
+        assertThat(races.waysToWin(15L, 40L)).isEqualTo(8);
+        assertThat(races.waysToWin(30L, 200L)).isEqualTo(9);
+        assertThat(races.waysToWin(71530L, 940200L)).isEqualTo(71503);
     }
 
     @Test
     void scoresRaces() {
-        Map<Integer, Integer> testRaces = Map.of(
-                7, 9,
-                15, 40,
-                30, 200
+        Map<Long, Long> testRaces = Map.of(
+                7L, 9L,
+                15L, 40L,
+                30L, 200L
         );
         assertThat(races.scoreRaces(testRaces)).isEqualTo(288);
     }
@@ -39,9 +39,12 @@ class Day06RacesTest {
     }
 
     @Test
-    void partTwo() {
-        assertThat(races.scoreRaces(Map.of(71530, 940200))).isEqualTo(71503);
+    void partTwoTest() {
+        assertThat(races.scoreRaces(Map.of(71530L, 940200L))).isEqualTo(71503);
     }
 
-
+    @Test
+    void partTwo() {
+        assertThat(races.scoreRaces(Map.of(47707566L, 282107911471062L))).isEqualTo(33875953);
+    }
 }
